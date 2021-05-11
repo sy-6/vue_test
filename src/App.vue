baccc5a324764b8087d0a11e373ecf794c7e9f40
sy-6 <249859693qq.com>
2021-05-11 16:44:33 +0800

    加box

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Box />
   </div>
 </template>
 
 <script>
+import Box from './components/Box.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Box
   }
 }
 </script>
